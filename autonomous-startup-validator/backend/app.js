e22266d37bd3578318430292bb43d7b390d533cc
nabhya8013 <nabhyamaheshwari8013@gmail.com>
2025-06-05 19:55:16 +0530

    Update app.js

---
+++ autonomous-startup-validator/backend/app.js
@@ -0,0 +1,3 @@
+app.use('/api/processing-data', require('./routes/processing'));
+app.use('/api/validation', require('./routes/validation'));
+app.use('/api/validation-results', require('./routes/validationResults'));
